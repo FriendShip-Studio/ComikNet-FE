46c9e92a7543054801039b44e0dff3c316f5f197
AsakiRain <1287059631@qq.com>
2023-01-26 17:05:51 +0800

    chore: user没东西老是报错，先放个页面

---
+++ src/views/UserControl/user.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main" :style="{ width: $route.meta.expand ? '100%' : '1200px' }">
+    <div class="content">
+      <div class="content-title">USER</div>
+      <a-spin
+        tip="再等会，要出来了"
+        :spinning="!loaded"
+        class="content-spin"
+      ></a-spin>
+      <div id="lightbox"></div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import useToggle from "@/utils/useToggle";
+
+const { val: loaded, set: setLoaded } = useToggle(false);
+</script>
+
+<style scoped></style>
