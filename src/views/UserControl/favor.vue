bcdefed31088b9d0d37bb799ff346f61b348e983
AsakiRain <1287059631@qq.com>
2023-01-26 16:45:23 +0800

    chore: favor页面类型完善; feat: favor页面加载动画

--- src/views/UserControl/favor.vue
+++ src/views/UserControl/favor.vue
@@ -2,9 +2,9 @@
   <main class="main" :style="{ width: $route.meta.expand ? '100%' : '1200px' }">
     <div class="content">
       <div class="content-title">收藏夹</div>
-      <div id="fav-list">
-        <Albums :album-list="favList" id="fav-list" />
-      </div>
+      <a-spin tip="再等会，要出来了" :spinning="!loaded" class="content-spin">
+      </a-spin>
+      <Albums :album-list="favList" id="fav-list" />
     </div>
   </main>
 </template>
@@ -15,17 +15,22 @@ import { useRouter } from "vue-router";
 import user from "@/apis/utils/userControl";
 import Albums from "@/components/ComicCards/AlbumList.vue";
 import type { AlbumInfo } from "@/models/albums";
+import useToggle from "@/utils/useToggle";
 
 const router = useRouter();
+const { val: loaded, set: setLoaded } = useToggle(false);
 const favList = ref<Array<AlbumInfo>>();
 
 const getFavourite = async () => {
   try {
-    const res = await user.getFavor();
-    favList.value = res.data?.list;
+    setLoaded(false);
+    const data = await user.getFavor();
+    favList.value = data.list;
     // eslint-disable-next-line
   } catch (error: any) {
     router.push("/login");
+  } finally {
+    setLoaded(true);
   }
 };
 
