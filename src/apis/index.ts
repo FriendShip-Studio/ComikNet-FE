fc2d1a16c4548f257f392aa146a42df34e948538
AsakiRain <1287059631@qq.com>
2023-01-26 13:22:30 +0800

    fix: 请求超时时的错误提示

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,61 +3,54 @@ import type { AxiosRequestConfig, AxiosResponse } from "axios";
 import { message } from "ant-design-vue";
 import { ApiRequest, ImageListRequest, MirrorRequest } from "@/models/requests";
 
-
 const requests = axios.create({
-    baseURL: "http://localhost:8000",
-    timeout: 30000,
-    withCredentials: true,
-    headers: {}
+  baseURL: "http://localhost:8000",
+  timeout: 30000,
+  withCredentials: true,
+  headers: {},
 });
 
 requests.interceptors.request.use(
-    (config: AxiosRequestConfig) => {
-        return config;
-    },
-    (error: any) => {
-        console.log(error);
-        return Promise.reject(error);
-    }
+  (config: AxiosRequestConfig) => {
+    return config;
+  },
+  (error: any) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
 );
 
 requests.interceptors.response.use(
-    (response: AxiosResponse) => {
-        return response.data;
-    },
-    (error: any) => {
-        const warn = `${error.response.status} ${error.response.statusText}`;
-        message.error(warn);
-        return Promise.reject(warn);
+  (response: AxiosResponse) => {
+    return response.data;
+  },
+  (err: any) => {
+    console.log(err);
+    let warn;
+    if (err.response) {
+      warn = `${err.response.status}: ${err.response.statusText}`;
+    } else {
+      warn = `${err.code}: ${err.message}`;
     }
+    message.error(warn);
+    return Promise.reject(warn);
+  }
 );
 
-const get = (
-    url: string,
-    params?: any,
-    config?: any
-): Promise<ApiRequest> => {
-    return requests.get(url, { params, ...config });
-}
+const get = (url: string, params?: any, config?: any): Promise<ApiRequest> => {
+  return requests.get(url, { params, ...config });
+};
 
-const post = (
-    url: string,
-    data?: any,
-    config?: any
-): Promise<ApiRequest> => {
-    return requests.post(url, data, { ...config });
-}
+const post = (url: string, data?: any, config?: any): Promise<ApiRequest> => {
+  return requests.post(url, data, { ...config });
+};
 
-const mirror = (
-    params?: any
-): Promise<MirrorRequest> => {
-    return requests.get("/mirror", { params });
-}
+const mirror = (params?: any): Promise<MirrorRequest> => {
+  return requests.get("/mirror", { params });
+};
 
-const img_list = (
-    params: string
-): Promise<ImageListRequest> => {
-    return requests.get("/img_list", { params });
-}
+const img_list = (params: string): Promise<ImageListRequest> => {
+  return requests.get("/img_list", { params });
+};
 
 export { get, post, mirror, img_list };
